116c00e8df19fe338414948559c9f43975e83120
Chalo Holidays <chaloholidays75@gmail.com>
2025-09-02 16:19:13 +0530

    new changes

---
+++ Services/IAuthService.cs
@@ -0,0 +1,11 @@
+using HotelAPI.Models.DTO;
+
+public interface IAuthService
+{
+    Task<AuthResponse> RegisterAsync(RegisterRequest request);
+    Task<AuthResponse> LoginAsync(LoginRequest request);
+    Task SendForgotPasswordEmailAsync(ForgotPasswordRequest request);
+    Task ResetPasswordAsync(ResetPasswordRequest request);
+    Task<AuthResponse> RefreshTokenAsync(RefreshTokenRequest request);
+    Task SendEmailAsync(string toEmail, string subject, string body);
+}
