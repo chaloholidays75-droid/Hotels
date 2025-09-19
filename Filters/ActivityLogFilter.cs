302b5c2e18c06751542a2503b5d9fbcdabf2fda1
ChaloHolidays <chaloholidays75@gmail.com>
2025-09-19 15:28:45 +0530

    new changes

--- Filters/ActivityLogFilter.cs
+++ Filters/ActivityLogFilter.cs
@@ -1,10 +1,7 @@
 using Microsoft.AspNetCore.Mvc.Filters;
 using HotelAPI.Data;
 using HotelAPI.Models;
-using Microsoft.AspNetCore.Http;
-using System.Linq;
-using System.Threading.Tasks;
-using System;
+using System.Security.Claims;
 
 namespace HotelAPI.Filters
 {
@@ -23,59 +20,64 @@ namespace HotelAPI.Filters
         {
             var resultContext = await next();
 
-            if (resultContext.Exception != null) return; // Only log successful actions
-
-            var httpMethod = resultContext.HttpContext.Request.Method;
-            string actionType = httpMethod switch
+            if (resultContext.Exception == null) // only log successful actions
             {
-                "POST" => "Created",
-                "PUT" => "Updated",
-                "PATCH" => "Updated",
-                "DELETE" => "Deleted",
-                _ => ""
-            };
+                var httpMethod = resultContext.HttpContext.Request.Method;
+                string actionType = httpMethod switch
+                {
+                    "POST" => "Created",
+                    "PUT" => "Edited",
+                    "PATCH" => "Edited",
+                    "DELETE" => "Deleted",
+                    _ => ""
+                };
 
-            if (string.IsNullOrEmpty(actionType)) return;
+                if (!string.IsNullOrEmpty(actionType))
+                {
+                    var entityName = resultContext.Controller.GetType().Name.Replace("Controller", "");
 
-            var username = _httpContextAccessor.HttpContext?.User?.FindFirst("FullName")?.Value ?? "Unknown user";
+                    // Get the username from JWT claim
+                    var username = _httpContextAccessor.HttpContext?.User?.Claims
+                        .FirstOrDefault(c => c.Type == "FullName")?.Value
+                        ?? _httpContextAccessor.HttpContext?.User?.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Email)?.Value
+                        ?? "Unknown user";
 
-            int entityId = 0;
-            string entityType = "";
-            string entityName = "";
+                    int entityId = 0;
+                    string entityLabel = "";
 
-            // Detect entity from action arguments
-            var firstArg = context.ActionArguments.Values.FirstOrDefault();
-            if (firstArg is Agency agency)
-            {
-                entityId = agency.Id;
-                entityType = "Agency";
-                entityName = agency.AgencyName;
+                    // Extract entity info from action arguments
+                    foreach (var arg in context.ActionArguments.Values)
+                    {
+                        if (arg is HotelAPI.Models.Agency agency)
+                        {
+                            entityId = agency.Id;
+                            entityLabel = agency.AgencyName;
+                        }
+                        else if (arg is HotelAPI.Models.HotelInfo hotel)
+                        {
+                            entityId = hotel.Id;
+                            entityLabel = hotel.HotelName;
+                        }
+                        else if (arg is int id) // id from route
+                        {
+                            entityId = id;
+                        }
+                    }
+
+                    var log = new RecentActivity
+                    {
+                        Username = username,
+                        ActionType = actionType,
+                        Entity = entityName,
+                        EntityId = entityId,
+                        Description = $"{username} {actionType.ToLower()} {entityName} \"{entityLabel}\"",
+                        CreatedAt = DateTime.UtcNow
+                    };
+
+                    _context.RecentActivities.Add(log);
+                    await _context.SaveChangesAsync();
+                }
             }
-            else if (firstArg is HotelInfo hotel)
-            {
-                entityId = hotel.Id;
-                entityType = "Hotel";
-                entityName = hotel.HotelName;
-            }
-            else if (context.ActionArguments.ContainsKey("id"))
-            {
-                entityId = (int)context.ActionArguments["id"];
-                entityType = context.Controller.GetType().Name.Replace("Controller", "");
-                entityName = ""; // optional, could query DB if needed
-            }
-
-            var log = new RecentActivity
-            {
-                Username = username,
-                ActionType = actionType,
-                Entity = entityType,
-                EntityId = entityId,
-                Description = $"{username} {actionType.ToLower()} {entityType} \"{entityName}\"",
-                CreatedAt = DateTime.UtcNow
-            };
-
-            _context.RecentActivities.Add(log);
-            await _context.SaveChangesAsync();
         }
     }
 }
