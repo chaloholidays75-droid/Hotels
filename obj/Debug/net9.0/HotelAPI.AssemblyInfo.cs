68e186d120f37e008a12fb840364a507bea21bb7
ChaloHolidays <chaloholidays75@gmail.com>
2025-09-20 15:21:31 +0530

    new changes

--- obj/Debug/net9.0/HotelAPI.AssemblyInfo.cs
+++ obj/Debug/net9.0/HotelAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70a619df995859108b6d33d28c65d799f01c5eb3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc15967f151ac5ab0dc6e99c29fdfe233535e445")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
