979737b130b9ab1f2855373a185dfb064e19ee6d
ChaloHolidays <chaloholidays75@gmail.com>
2025-09-24 01:14:10 +0530

    new changes

--- Controllers/RecentActivityController.cs
+++ Controllers/RecentActivityController.cs
@@ -8,7 +8,7 @@ using System.Linq;
 namespace HotelAPI.Controllers
 {
     [ApiController]
-    [Route("api/recent")]  // <-- this is the endpoint
+    [Route("api/recent")]
     public class RecentActivitiesController : ControllerBase
     {
         private readonly AppDbContext _context;
@@ -17,16 +17,18 @@ namespace HotelAPI.Controllers
         {
             _context = context;
         }
-        [HttpGet("list")]
-        
-            public async Task<IActionResult> GetRecentlistActivities()
-            {
-                var activities = await _context.RecentActivities
-                    .ToListAsync();
 
-                return Ok(activities);
-            }
-        
+        // GET: api/recent/list
+        [HttpGet("list")]
+        public async Task<IActionResult> GetRecentListActivities()
+        {
+            var activities = await _context.RecentActivities
+                .OrderByDescending(a => a.Timestamp)
+                .ToListAsync();
+
+            return Ok(activities);
+        }
+
         // GET: api/recent?page=1&pageSize=20
         [HttpGet]
         public async Task<IActionResult> GetRecentActivities(int page = 1, int pageSize = 20)
@@ -39,6 +41,67 @@ namespace HotelAPI.Controllers
 
             return Ok(activities);
         }
+
+        // POST: api/recent/fix-old-activities
+        [HttpPost("fix-old-activities")]
+        public async Task<IActionResult> FixOldActivities()
+        {
+            var users = await _context.Users.ToDictionaryAsync(u => u.Id, u => u.Email);
+
+            // Fix Agencies
+            var agencies = await _context.Agencies.ToListAsync();
+            foreach (var agency in agencies)
+            {
+                var activities = _context.RecentActivities
+                    .Where(r => r.Entity == "Agencies" && r.EntityId == agency.Id && r.UserId == 0);
+
+                foreach (var act in activities)
+                {
+                    if (act.Action == "CREATE" && agency.CreatedById.HasValue)
+                    {
+                        act.UserId = agency.CreatedById.Value;
+                        act.UserName = users.ContainsKey(agency.CreatedById.Value)
+                            ? users[agency.CreatedById.Value]
+                            : "Legacy User";
+                    }
+                    else if ((act.Action == "UPDATE" || act.Action == "DELETE") && agency.UpdatedById.HasValue)
+                    {
+                        act.UserId = agency.UpdatedById.Value;
+                        act.UserName = users.ContainsKey(agency.UpdatedById.Value)
+                            ? users[agency.UpdatedById.Value]
+                            : "Legacy User";
+                    }
+                }
+            }
+
+            // Fix HotelInfo
+            var hotels = await _context.HotelInfo.ToListAsync();
+            foreach (var hotel in hotels)
+            {
+                var activities = _context.RecentActivities
+                    .Where(r => r.Entity == "HotelInfo" && r.EntityId == hotel.Id && r.UserId == 0);
+
+                foreach (var act in activities)
+                {
+                    if (act.Action == "CREATE" && hotel.CreatedById.HasValue)
+                    {
+                        int createdById = hotel.CreatedById.Value;
+                        act.UserId = createdById;
+                        act.UserName = users.ContainsKey(createdById) ? users[createdById] : "Legacy User";
+                    }
+                    else if ((act.Action == "UPDATE" || act.Action == "DELETE") && hotel.UpdatedById.HasValue)
+                    {
+                        int updatedById = hotel.UpdatedById.Value;
+                        act.UserId = updatedById;
+                        act.UserName = users.ContainsKey(updatedById) ? users[updatedById] : "Legacy User";
+                    }
+
+                }
+            }
+
+            await _context.SaveChangesAsync();
+
+            return Ok(new { message = "Old RecentActivity records updated successfully!" });
+        }
     }
 }
-
