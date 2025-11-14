9abf81948a8b1207a4d8612845316a13da43fdd3
ChaloHolidays <chaloholidays75@gmail.com>
2025-11-14 12:32:30 +0530

    new changes

--- Models/DTO/BookingRoomDto.cs
+++ Models/DTO/BookingRoomDto.cs
@@ -7,7 +7,7 @@ namespace HotelAPI.Models.DTO
         public int RoomTypeId { get; set; }
         public int Adults { get; set; }
         public int Children { get; set; }
-        public List<int> ChildrenAges { get; set; } = new List<int>();
+        public List<int>? ChildrenAges { get; set; } = new List<int>();
         public string? Inclusion { get; set; }
         public string? LeadGuestName { get; set; }
         public List<string>? GuestNames { get; set; } = new();
